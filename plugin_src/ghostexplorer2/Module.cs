501fd853d11dcadb3b3c5ba739a4260ce6113720
Dice <tetradice@gmail.com>
2020-06-03 22:59:21 +0900

    yaya → SakuraBridge

---
+++ plugin_src/ghostexplorer2/Module.cs
@@ -0,0 +1,36 @@
+﻿using SakuraBridge.Library;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostExplorer2
+{
+    public class Module : PluginModule
+    {
+        /// <summary>
+        /// プラグインのバージョン (versionリクエストに対して返す値)
+        /// </summary>
+        public override string Version
+        {
+            get { return "GhostExplorer-0.4.0"; }
+        }
+
+        /// <summary>
+        /// メニューからの実行
+        /// </summary>
+        public override PluginResponse OnMenuExec(PluginRequest req)
+        {
+            var res = PluginResponse.OK();
+            res.Event = "OnGhostExplorer2Open";
+
+            var ghostId = req.References[3];
+            Process.Start(Path.Combine(DLLDirPath, @"gui\GhostExplorer2GUI.exe"), "id:" + ghostId);
+
+            return res;
+        }
+    }
+}
