906e4993ed121c03bd14fd7854483569a42e8921
Dice <tetradice@gmail.com>
2020-06-06 00:38:48 +0900

    shellexplorer2 SakuraBridgeに移行

---
+++ plugin_src/shellexplorer2/Module.cs
@@ -0,0 +1,29 @@
+﻿using SakuraBridge.Library;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellExplorer2
+{
+    public class Module : PluginModule
+    {
+        /// <summary>
+        /// メニューからの実行
+        /// </summary>
+        public override PluginResponse OnMenuExec(PluginRequest req)
+        {
+            var res = PluginResponse.OK();
+            res.Event = "OnShellExplorer2Open";
+
+            var ghostId = req.References[3];
+            var ghostDirPath = req.References[4].TrimEnd('\\');
+            Process.Start(Path.Combine(DLLDirPath, @"gui\ShellExplorer2GUI.exe"), "id:" + ghostId + @" """ + ghostDirPath + @"""");
+
+            return res;
+        }
+    }
+}
