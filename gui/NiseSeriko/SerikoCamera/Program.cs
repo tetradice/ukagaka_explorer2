a66027a11d4ce84895e8ff5c8fda2b14f6bf8da0
Dice <tetradice@gmail.com>
2020-06-14 17:43:07 +0900

    対象フレームワークを変更

--- gui/NiseSeriko/SerikoCamera/Program.cs
+++ gui/NiseSeriko/SerikoCamera/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
 using System.Text;
 using CommandLine;
